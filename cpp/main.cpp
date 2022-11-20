41b236f3f45896bb222458e23fdc6f53d7e53e02
vlad <vlad.genyuk@mail.ru>
2022-11-20 18:01:20 +0300

    added main.cpp

---
+++ cpp/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Laboratory work #8. GIT\n";
+    cout << "Variant #0. Library Subscription\n";
+    cout << "Author: Sergey Yermochenko\n";
+    cout << "Group: XX\n";
+    return 0;
+}
