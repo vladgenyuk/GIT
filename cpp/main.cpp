e886b111884d240f8953f7ddae90b1981f028194
vlad <vlad.genyuk@mail.ru>
2022-11-20 17:58:46 +0300

    Formatting data in output to console

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -17,21 +18,48 @@ int main()
     try
     {
         read("data.txt", subscriptions, size);
+        cout << "***** Библиотечный абонемент *****\n\n";
         for (int i = 0; i < size; i++)
         {
-            cout << subscriptions[i]->reader.last_name << '\n';
-            cout << subscriptions[i]->reader.first_name << '\n';
-            cout << subscriptions[i]->reader.middle_name << '\n';
-            cout << subscriptions[i]->finish.day << ' ';
-            cout << subscriptions[i]->finish.month << ' ';
-            cout << subscriptions[i]->finish.year << '\n';
-            cout << subscriptions[i]->start.day << ' ';
-            cout << subscriptions[i]->start.month << ' ';
-            cout << subscriptions[i]->start.year << '\n';
-            cout << subscriptions[i]->author.last_name << '\n';
-            cout << subscriptions[i]->author.first_name << '\n';
-            cout << subscriptions[i]->author.middle_name << '\n';
-            cout << subscriptions[i]->title << '\n';
+            /********** вывод читателя **********/
+            cout << "Читатель........: ";
+            // вывод фамилии
+            cout << subscriptions[i]->reader.last_name << " ";
+            // вывод первой буквы имени
+            cout << subscriptions[i]->reader.first_name[0] << ". ";
+            // вывод первой буквы отчества
+            cout << subscriptions[i]->reader.middle_name[0] << ".";
+            cout << '\n';
+            /********** вывод книги **********/
+            cout << "Книга...........: ";
+            // вывод фамилии автора
+            cout << subscriptions[i]->author.last_name << " ";
+            // вывод первой буквы имени автора
+            cout << subscriptions[i]->author.first_name[0] << ". ";
+            // вывод первой буквы отчества автора
+            cout << subscriptions[i]->author.middle_name[0] << ".";
+            cout << ", ";
+            // вывод названия
+            cout << '"' << subscriptions[i]->title << '"';
+            cout << '\n';
+            /********** вывод даты выдачи **********/
+            // вывод года
+            cout << "Дата выдачи.....: ";
+            cout << setw(4) << setfill('0') << subscriptions[i]->start.year << '-';
+            // вывод месяца
+            cout << setw(2) << setfill('0') << subscriptions[i]->start.month << '-';
+            // вывод числа
+            cout << setw(2) << setfill('0') << subscriptions[i]->start.day;
+            cout << '\n';
+            /********** вывод даты возврата **********/
+            // вывод года
+            cout << "Дата возврата...: ";
+            cout << setw(4) << setfill('0') << subscriptions[i]->finish.year << '-';
+            // вывод месяца
+            cout << setw(2) << setfill('0') << subscriptions[i]->finish.month << '-';
+            // вывод числа
+            cout << setw(2) << setfill('0') << subscriptions[i]->finish.day;
+            cout << '\n';
             cout << '\n';
         }
         for (int i = 0; i < size; i++)
